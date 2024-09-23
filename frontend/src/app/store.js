30ffa0c20452ed15efdf95b706be99508cb5d17c
Yaron Ender <yaron.ender@gmail.com>
2024-09-24 00:01:30 +0300

    delete files

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,15 +1,12 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import postsReducer from '../features/posts/postsSlice'
-// import notificationsReducer from '../features/notifications/notificationsSlice'
-// import { apiSlice } from '../api/apiSlice'
+
 
 export default configureStore({
     reducer: {
       posts: postsReducer,
-      // notifications: notificationsReducer,
-      // [apiSlice.reducerPath]: apiSlice.reducer
+    
     },
-      // middleware: getDefaultMiddleware => 
-      // getDefaultMiddleware().concat(apiSlice.middleware)
+   
 })
 
--- frontend/src/features/posts/postsSlice.js
+++ frontend/src/features/posts/postsSlice.js
@@ -1,5 +1,4 @@
-import {createSlice, nanoid, createAsyncThunk, createSelector, 
-    createEntityAdapter} from '@reduxjs/toolkit'
+import {createSlice} from '@reduxjs/toolkit'
 
 
 const initialState = [
@@ -11,80 +10,19 @@ const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
-        reactionAdded(state,action) {
-            const {postId, reaction} = action.payload
-            const existingPost = state.entities[postId]
-            if(existingPost) {
-                existingPost.reactions[reaction]++
-            }
-        },
         postAdded(state, action){
         console.log(state.posts);
             state.push(action.payload) 
         },
-        // postAdded: {
-        //     reducer(state, action) {
-        //         state.posts.push(action.payload)
-        // },
-        //     prepare(title, content, userId) {
-        //         return {
-        //             payload: {
-        //                 id: nanoid(),
-        //                     date: new Date().toISOString(),
-        //                     title,
-        //                     content,
-        //                     // user: userId,
-        //                     // reactions: {
-        //                     //     thumbsUp: 0,
-        //                     //     raisingHands: 0,
-        //                     //     heart: 0,
-        //                     //     rocket: 0,
-        //                     //     eyes: 0
-        //                     // }
-        //             }
-        //         }
-        //     }
-        // },
-        postUpdated(state, action) {
-            const {id, title, content} = action.payload
-            const existingPost = state.entities[id]
-            if(existingPost) {
-                existingPost.title = title
-                existingPost.content = content
-            }
-        }
     },
-    // extraReducers(builder) {
-    //     builder
-    //     .addCase(fetchPosts.pending, (state, action) => {
-    //         state.status = 'loading'
-    //     })
-    //     .addCase(fetchPosts.fulfilled, (state, action) => {
-    //         state.status = 'succeeded'
-    //         postsAdapter.upsertMany(state, action.payload)
-    //     })
-    //     .addCase(fetchPosts.rejected, (state, action) => {
-    //         state.status = 'failed'
-    //         state.error = action.error.message
-    //     })
-    //     .addCase(addNewPost.fulfilled, postsAdapter.addOne)
-    // }
+ 
 })
 
-console.log('Initial state:', initialState); // Add this line
+console.log('Initial state:', initialState); 
 
-export const {postAdded, postUpdated, reactionAdded} = postsSlice.actions
+export const {postAdded} = postsSlice.actions
 
-// export const {
-//     selectAll: selectAllPosts,
-//     selectById: selectPostsById,
-//     selectIds: selectPostIds
-// } = postsAdapter.getSelectors(state => state.posts)
 
-// export const selectPostsByUser = createSelector(
-//     [selectAllPosts, (state, userId) => userId],
-//     (posts, userId) => posts.filter(post => post.user === userId)
-// )
 
 export default postsSlice.reducer;
 
